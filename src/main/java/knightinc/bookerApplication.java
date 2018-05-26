2a354dca7514d38d0eeff13f6f644d6759150ac8
kngmat004 <kngmat004@gmail.com>
2018-05-26 22:07:16 +0200

    create database if not exists

--- src/main/java/knightinc/bookerApplication.java
+++ src/main/java/knightinc/bookerApplication.java
@@ -7,8 +7,18 @@ import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import knightinc.core.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class bookerApplication extends Application<bookerConfiguration> {
+    private final static Logger logger = LoggerFactory.getLogger(bookerApplication.class);
 
     public static void main(final String[] args) throws Exception {
         new bookerApplication().run(args);
@@ -26,7 +36,6 @@ public class bookerApplication extends Application<bookerConfiguration> {
         }
     };
 
-
     @Override
     public void initialize(final Bootstrap<bookerConfiguration> bootstrap) {
         bootstrap.addBundle(hibernate);
@@ -41,6 +50,49 @@ public class bookerApplication extends Application<bookerConfiguration> {
     @Override
     public void run(final bookerConfiguration configuration,
                     final Environment environment) {
+        logger.info("Application start...");
+        try {
+            createDatabaseIfNotExists(configuration);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        }
+
+    }
+
+    private void createDatabaseIfNotExists(final bookerConfiguration configuration) throws ClassNotFoundException, SQLException {
+
+        String databaseName = "bookerdb";
+        String port = "5444";
+
+        String user = configuration.getDatabaseAppDataSourceFactory().getUser();
+        String password = configuration.getDatabaseAppDataSourceFactory().getPassword();
+
+        String connectionString = String.format("jdbc:postgresql://localhost:%s/", port);
+        logger.info("Create database if not exists...{}", databaseName);
+        logger.info(connectionString);
+
+        Class.forName("org.postgresql.Driver");
+        Connection connection = null;
+        connection = DriverManager.getConnection(connectionString,user, password);
+
+        ResultSet executeQuery = connection.createStatement().executeQuery("SELECT datname FROM pg_database;");
+
+        List<String> tables = new ArrayList<>();
+
+        while(executeQuery.next()) {
+            tables.add(executeQuery.getString(1));
+        }
+
+        if(!tables.contains(databaseName.toLowerCase())) {
+            logger.info(databaseName + " does not exist... creating");
+            connection.createStatement().execute(String.format("Create database %s;", databaseName));
+        } else {
+            logger.info(databaseName + " already exists");
+        }
+        connection.close();
     }
 
 }
